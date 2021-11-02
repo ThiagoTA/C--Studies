2a8938bad7a2a90fa502a5325fc6c26aebdb158c
Thiago Teixeira <60077182+ThiagoTA@users.noreply.github.com>
2021-11-02 14:37:41 -0300

    Add files via upload

---
+++ ExProposto6.1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExProposto6._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com o valor de 'A', 'B' e 'C':");
+            string[] vet = Console.ReadLine().Split(' ');
+            double a = double.Parse(vet[0]);
+            double b = double.Parse(vet[1]);
+            double c = double.Parse(vet[2]);
+
+            double triangulo = (a * c) / 2;
+            double circulo = 3.14159 * c * c;
+            double trapezio = (a + b) / 2 * c;
+            double quadrado = b * b;
+            double retangulo = a * b;
+
+            Console.WriteLine("TRIANGULO: " + triangulo.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("CIRCULO: " + circulo.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("TRAPEZIO: " + trapezio.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("QUADRADO: " + quadrado.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("RETANGULO: " + retangulo.ToString("F3", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ ExProposto6.2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ExProposto6._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com um valor:");
+            double num = double.Parse(Console.ReadLine());
+
+            if (num < 0 || num > 100)
+            {
+                Console.WriteLine("Fora de Intervalo");
+            }
+            else if (num <= 25.0)
+            {
+                Console.WriteLine("Intervalo [0, 25]");
+            }
+            else if (num <= 50.0)
+            {
+                Console.WriteLine("Intervalo [25, 50]");
+            }
+            else if (num <= 75.0)
+            {
+                Console.WriteLine("Intervalo [50, 75]");
+            }
+            else if (num <= 100)
+            {
+                Console.WriteLine("Intervalo [75, 100]");
+            } 
+        }
+    }
+}
