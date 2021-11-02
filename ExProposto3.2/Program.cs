962813731bbbf74b4a2aaa82a15abcf4f191d366
Thiago Teixeira <60077182+ThiagoTA@users.noreply.github.com>
2021-11-02 14:35:22 -0300

    Add files via upload

---
+++ ExProposto3.2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ExProposto3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com o valor de 'A'");
+            int A = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre com o valor de 'B");
+            int B = int.Parse(Console.ReadLine());
+
+            if (A % B == 0 || B % A == 0)
+            {
+                Console.WriteLine("São Multiplos");
+            } else
+            {
+                Console.WriteLine("Não são Multiplos");
+            }
+        }
+    }
+}
