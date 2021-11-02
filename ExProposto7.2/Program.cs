6071f89f1757a34030ff448d5ecdbba3652e572b
Thiago Teixeira <60077182+ThiagoTA@users.noreply.github.com>
2021-11-02 14:38:42 -0300

    Add files via upload

---
+++ ExProposto7.2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ExProposto7._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com o valor do X e do Y");
+            string[] num = Console.ReadLine().Split(' ');
+            double X = double.Parse(num[0]);
+            double Y = double.Parse(num[1]);
+
+            if (X == 0 && Y == 0)
+            {
+                Console.WriteLine("Origem");
+            } else if (X > 0 && Y > 0)
+            {
+                Console.WriteLine("Q1");
+            } else if (X < 0 && Y > 0)
+            {
+                Console.WriteLine("Q2");
+            } else if (X < 0 && Y < 0)
+            {
+                Console.WriteLine("Q3");
+            } else if (X > 0 && Y < 0)
+            {
+                Console.WriteLine("Q4");
+            }
+        }
+    }
+}
---
+++ ExProposto8.2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExProposto8._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com o valor do salário:");
+            double salary = double.Parse(Console.ReadLine());
+
+            double imposto;
+            if (salary <= 2000.0)
+            {
+                imposto = 0.0;
+            } else if (salary <= 3000.0)
+            {
+                imposto = (salary - 2000.00) * 0.08;
+            } else if (salary <= 4500.0) 
+            {
+                imposto = (salary - 3000.00) * 0.18 + 1000.0 * 0.08;
+            } else
+            {
+                imposto = (salary - 4500.00) * 0.28 + 1500.0 * 0.18 + 1000.0 * 0.08;
+            }
+
+            if (imposto == 0.0)
+            {
+                Console.WriteLine("Isento");
+            } else
+            {
+                Console.WriteLine("R$ " + imposto.ToString("F2", CultureInfo.InvariantCulture));
+            }
+       
+        }
+    }
+}
