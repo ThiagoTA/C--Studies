2899097c0d1a4426ea78ce37de6665a1634607ed
Thiago Teixeira <60077182+ThiagoTA@users.noreply.github.com>
2021-11-02 14:36:09 -0300

    Add files via upload

---
+++ ExProposto4.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExProposto4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com o número do funcionário");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o múmero de horas trabalhadas");
+            int hours = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ente com o salário desse funcionário");
+            double money = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double salary = money * hours ;
+
+            Console.WriteLine("NUMBER = {0}", number);
+            Console.WriteLine($"SALARY = U$ {salary.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
---
+++ ExProposto4.2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ExProposto4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hora Inicial:");
+            int start = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Hora Final:");
+            int end = int.Parse(Console.ReadLine());
+
+            int total;
+
+            if (start < end)
+            {
+                total = end - start;
+            } else
+            {
+                total = 24 - start - end;
+            }
+
+            Console.WriteLine("O JOGO DUROU {0} HORA(S)", total);
+        }
+    }
+}
