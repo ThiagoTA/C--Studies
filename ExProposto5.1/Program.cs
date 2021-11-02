95dfa34e02ae7167aa00b3eada75ff3396b8f9ca
Thiago Teixeira <60077182+ThiagoTA@users.noreply.github.com>
2021-11-02 14:36:58 -0300

    Add files via upload

---
+++ ExProposto5.1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExProposto5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o valor código da peça, o número de peças e o valor unitário");
+            string[] vet1 = Console.ReadLine().Split(' ');
+            int cod1 = int.Parse(vet1[0]);
+            int quant1 = int.Parse(vet1[1]);
+            double preco1 = double.Parse(vet1[2]);
+
+            double resultado1 = preco1 * quant1;
+
+            Console.WriteLine("Digite o valor código da peça, o número de peças e o valor unitário");
+            string[] vet2 = Console.ReadLine().Split(' ');
+            int cod2 = int.Parse(vet2[0]);
+            int quant2 = int.Parse(vet2[1]);
+            double preco2 = double.Parse(vet2[2]);
+
+            double resultado2 = preco2 * quant2;
+
+            double resultadoFinal = resultado1 + resultado2;
+
+            Console.WriteLine("VALOR A PAGAR: " + resultadoFinal.ToString("F2", CultureInfo.InvariantCulture));
+
+
+        }
+    }
+}
---
+++ ExProposto5.2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ExProposto5._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com o código e a quantidade do item:");
+            string[] item = Console.ReadLine().Split(' ');
+            int cod = int.Parse(item[0]);
+            int quant = int.Parse(item[1]);
+
+            double total;
+
+            if (cod == 1)
+            {
+                total = quant * 4.0;
+            } else if (cod == 2)
+            {
+                total = quant * 4.5;
+            } else if (cod == 3)
+            {
+                total = quant * 5.0;
+            } else if (cod == 4)
+            {
+                total = quant * 2.0;
+            } else 
+            {
+                total = quant * 1.5;
+            }
+
+            Console.WriteLine("Total: R$ " + total.ToString("F2"));
+
+        }
+    }
+}
