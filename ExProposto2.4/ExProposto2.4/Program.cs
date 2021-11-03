a3ff58ad428c32f7ccf962ef4920b6e7c0af951c
Thiago Teixeira <60077182+ThiagoTA@users.noreply.github.com>
2021-11-03 14:01:51 -0300

    Add files via upload

---
+++ ExProposto2.4/ExProposto2.4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ExProposto2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entre com valor de N: ");
+            int N = int.Parse(Console.ReadLine());
+
+            int entrada = 0;
+            int saida = 0;
+
+            for (int i = 1; i <= N; i++)
+            {
+                int valor = int.Parse(Console.ReadLine());
+                if (valor < 10 || valor > 20)
+                {
+                    saida += 1;
+                } else
+                {
+                    entrada += 1;
+                }
+            }
+            Console.WriteLine("\n"+ entrada + " in");
+            Console.WriteLine(saida + " out");
+        }
+    }
+}
