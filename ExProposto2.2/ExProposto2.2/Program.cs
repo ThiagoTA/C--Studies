6cc92f07677685329463393a69c19f421c904d1d
Thiago Teixeira <60077182+ThiagoTA@users.noreply.github.com>
2021-11-03 14:03:42 -0300

    Add files via upload

---
+++ ExProposto2.2/ExProposto2.2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ExProposto2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com um número\n");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("PAR");
+            } else
+            {
+                Console.WriteLine("ÍMPAR");
+            }
+        }
+    }
+}
