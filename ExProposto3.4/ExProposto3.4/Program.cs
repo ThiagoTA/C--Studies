a6c1b9bcc625bdf8e4dd52c2ed94d86deeb86819
Thiago Teixeira <60077182+ThiagoTA@users.noreply.github.com>
2021-11-03 15:02:19 -0300

    Add files via upload

---
+++ ExProposto3.4/ExProposto3.4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExProposto3._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entre com o valor de N: ");
+            int N = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < N; i++)
+            {
+
+                Console.WriteLine("\nEntre com {0} médias:", N);
+                string[] notas = Console.ReadLine().Split(' ');
+                double a = double.Parse(notas[0]);
+                double b = double.Parse(notas[1]);
+                double c = double.Parse(notas[2]);
+
+                double soma = (a * 2.0 + b * 3.0 + c * 5.0) / 10;
+
+                Console.WriteLine("\n" + soma.ToString("F1", CultureInfo.InvariantCulture));
+            }   
+        }
+    }
+}
