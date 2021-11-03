b5e922165a8d7f46705af6451778ae1025d67fb7
Thiago Teixeira <60077182+ThiagoTA@users.noreply.github.com>
2021-11-03 14:02:32 -0300

    Add files via upload

---
+++ ExProposto1.4/ExProposto1.4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ExProposto1._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entre com o valor: ");
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= num; i++)
+            {
+                if(i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
