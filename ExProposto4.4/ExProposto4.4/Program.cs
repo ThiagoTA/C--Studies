287f5da934988c6e4f84df75772a2d82d0b56383
Thiago Teixeira <60077182+ThiagoTA@users.noreply.github.com>
2021-11-03 15:13:26 -0300

    Add files via upload

---
+++ ExProposto4.4/ExProposto4.4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ExProposto4._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entre com o valor de N: ");
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine("\nEntre com dois valores");
+                string[] divisao = Console.ReadLine().Split(' ');
+                double a = double.Parse(divisao[0]);
+                double b = double.Parse(divisao[1]);
+
+                if (b == 0)
+                {
+                    Console.WriteLine("\nDivisão impossivel!");
+                } else
+                {
+                    double resultado = a / b;
+                    Console.WriteLine("\n" + resultado);
+                }
+            }
+        }
+    }
+}
