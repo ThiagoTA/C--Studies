48b3ed36eedbf321d8af3bbae6134867520dd582
Thiago Teixeira <60077182+ThiagoTA@users.noreply.github.com>
2021-11-04 13:57:48 -0300

    Add files via upload

---
+++ ExProposto7.4/ExProposto7.4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ExProposto7._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entre com o valor de N: ");
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= N; i++)
+            {
+                int quadrado = i * i;
+                int cubo = i * i * i;
+                Console.WriteLine("{0} {1} {2}", i, quadrado, cubo);
+            }
+        }
+    }
+}
