9a592ef7125f1a43e908dd81b05a1a9513b87134
Thiago Teixeira <60077182+ThiagoTA@users.noreply.github.com>
2021-11-05 15:06:05 -0300

    Add files via upload

---
+++ Classes, Atributos e Métodos/ExProposto1/ExProposto1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ExProposto1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo r = new Retangulo();
+
+            Console.WriteLine("Entre com a largura e altura do retângulo: ");
+            Console.Write("Largura: ");
+            r.Largura = double.Parse(Console.ReadLine());
+            Console.Write("Altura: ");
+            r.Altura = double.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine(r);
+        }
+    }
+}
---
+++ Classes, Atributos e Métodos/ExProposto1/ExProposto1/Retangulo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExProposto1
+{
+    class Retangulo
+    {
+        public double Largura;
+        public double Altura;
+
+        public double Area()
+        {
+            return Largura * Altura;
+        }
+
+        public double Perimreto()
+        {
+            return (Largura * Largura) + (Altura * Altura);
+        }
+
+        public double Diagonal()
+        {
+            return Math.Sqrt((Largura * Largura) + (Altura * Altura));
+        }
+
+        public override string ToString()
+        {
+            return "Area = "
+                + Area().ToString("F2")
+                + "\nPerímetro = "
+                + Perimreto().ToString("F2")
+                + "\nDiagonal = "
+                + Diagonal().ToString("F2");
+        }
+    }
+}
