926e4b552b3c927bc568b16b51500675ff872492
Thiago Teixeira <60077182+ThiagoTA@users.noreply.github.com>
2021-11-05 15:35:02 -0300

    Add files via upload

---
+++ Classes, Atributos e Métodos/ExProposto2/ExProposto2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ExProposto2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dados f = new Dados();
+
+            Console.WriteLine("Entre com os dados do Funcionário:");
+            Console.Write("Nome: ");
+            f.Nome = Console.ReadLine();
+            Console.Write("Salário bruto: ");
+            f.SalarioBruto = double.Parse(Console.ReadLine());
+            Console.Write("Imposto: ");
+            f.Imposto = double.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Funcionário: " + f);
+
+            Console.WriteLine();
+            Console.Write("Digite a porcentagem para aumentar o salário: ");
+            double pt = double.Parse(Console.ReadLine());
+            f.AumentarSalario(pt);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + f);
+        }
+    }
+}
