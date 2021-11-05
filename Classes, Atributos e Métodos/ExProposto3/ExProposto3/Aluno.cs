7e9d3af17421979bdf081b5c353fcb8094120b92
Thiago Teixeira <60077182+ThiagoTA@users.noreply.github.com>
2021-11-05 16:34:04 -0300

    Add files via upload

---
+++ Classes, Atributos e Métodos/ExProposto3/ExProposto3/Aluno.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExProposto3
+{
+    class Aluno
+    {
+        public string Nome;
+        public double Tr1, Tr2, Tr3;
+
+        public double NotaFinal()
+        {
+            return Tr1 + Tr2  + Tr3 ; 
+        }
+
+        public bool Aprovado()
+        {
+            if(NotaFinal() >= 60)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+
+        public double NotaFaltando()
+        {
+            return NotaFinal() - 60;
+        }   
+    }
+}
---
+++ Classes, Atributos e Métodos/ExProposto3/ExProposto3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ExProposto3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno a = new Aluno();
+
+            Console.Write("Nome do aluno: ");
+            a.Nome = Console.ReadLine();
+            Console.WriteLine("Digite as três notas do aluno:");
+            string[] vet = Console.ReadLine().Split(' ');
+            a.Tr1 = double.Parse(vet[0]);
+            a.Tr2 = double.Parse(vet[1]);
+            a.Tr3 = double.Parse(vet[2]);
+
+            Console.WriteLine();
+            Console.WriteLine("Nota final = " + a.NotaFinal().ToString("F2"));
+
+            if (a.Aprovado())
+            {
+                Console.WriteLine("Aprovado");
+            } else
+            {
+                Console.WriteLine("Reprovado");
+                Console.WriteLine("Faltaram " + a.NotaFaltando() + " pontos");
+            }
+        }
+    }
+}
