358e3e7ecb33ff93f6b9942332163628268955cb
Thiago Teixeira <60077182+ThiagoTA@users.noreply.github.com>
2021-11-08 14:24:54 -0300

    Add files via upload

---
+++ Método Static/ExProposto/ExProposto/ConversorDeMoeda.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExProposto
+{
+    class ConversorDeMoeda
+    {
+        public static double IOF = 6.0; 
+
+        public static double ConverterMoeda(double dolar, double comprar)
+        {
+            double total = dolar * comprar;
+            return total + total * IOF / 100.0;
+        }
+    }
+}
---
+++ Método Static/ExProposto/ExProposto/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExProposto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual é a cotação do dólar? ");
+            double dolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantos dólares você vai comprar? ");
+            double comprar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double valor = ConversorDeMoeda.ConverterMoeda(dolar, comprar);
+
+            Console.WriteLine("\nValor a ser pago em reais = " + valor.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
