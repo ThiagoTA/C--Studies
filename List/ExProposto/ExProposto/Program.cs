b039233ff9d291d8db93d79d81996e5f40782b88
Thiago Teixeira <60077182+ThiagoTA@users.noreply.github.com>
2021-11-30 15:25:08 -0300

    Add files via upload

---
+++ List/ExProposto/ExProposto/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExProposto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Emplyoee>emplyoeeId;
+
+            Console.Write("Entre com o número de funcionários para registrar: ");
+            int start = int.Parse(Console.ReadLine());
+
+            emplyoeeId = new List<Emplyoee>();
+
+            for (int i = 1; i <= start; i++)
+            {
+                Console.WriteLine("\nEmployee #" + i + ":");
+                Console.Write("ID: ");
+                int id = int.Parse(Console.ReadLine());
+                if (emplyoeeId.Find(x => x.Id == id) == null)
+                {
+                    Console.Write("Name: ");
+                    string name = Console.ReadLine();
+                    Console.Write("Salary: ");
+                    double salary = double.Parse(Console.ReadLine());
+                    emplyoeeId.Add(new Emplyoee(id, name, salary));
+                } else
+                {
+                    Console.WriteLine("Esse 'id' já existe");
+                }
+            }
+            Console.Write("\nInsira o 'id' do funcionário que terá aumento no salário: ");
+            int findid = int.Parse(Console.ReadLine());
+
+            if (emplyoeeId.Find(x => x.Id == findid) == null) {
+                Console.WriteLine("Esse ID não existe");
+            } else
+            {
+                Console.Write("Insira a porcentagem: ");
+                double percentage = double.Parse(Console.ReadLine());
+                emplyoeeId.Find(x => x.Id == findid).increaseSalary(percentage);
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Lista Atualizada:");
+            foreach (Emplyoee obj in emplyoeeId)
+            {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}
