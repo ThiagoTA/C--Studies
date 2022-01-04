aea1973c50606ab5c0834c4b6e6e15e3c0c89785
Thiago Teixeira <60077182+ThiagoTA@users.noreply.github.com>
2022-01-04 15:15:02 -0300

    Add files via upload

---
+++ Enumeração/ExProposto/ExProposto 2/ExProposto 2/Entities/Comment.cs
@@ -0,0 +1,16 @@
+﻿namespace ExProposto_2.Entities
+{
+    class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment()
+        {
+        }
+
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+}
