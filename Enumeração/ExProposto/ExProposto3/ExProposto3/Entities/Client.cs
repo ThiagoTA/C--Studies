a2374a0be3376aadd457df0f643b7209798bb3f5
Thiago Teixeira <60077182+ThiagoTA@users.noreply.github.com>
2022-01-05 17:01:13 -0300

    Add files via upload

---
+++ Enumeração/ExProposto/ExProposto3/ExProposto3/Entities/Client.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ExProposto3.Entities
+{
+    class Client
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime birthDate { get; set; }
+
+        public Client()
+        {
+        }
+
+        public Client(string name, string email, DateTime date)
+        {
+            Name = name;
+            Email = email;
+            Date = date;
+        }
+    }
+}
---
+++ Enumeração/ExProposto/ExProposto3/ExProposto3/Entities/Order.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ExProposto3.Entities.Enums;
+
+namespace ExProposto3.Entities
+{
+    class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public Client Client { get; set; }
+
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+        public Order()
+        {
+        }
+
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
+            Moment = moment;
+            Status = status;
+            Client = client;
+        }
+
+        public void addItem(OrderItem item)
+        {
+            Items.Add(item);
+        }
+
+        public void RemoveItem(OrderItem item)
+        {
+            Items.Remove(item);
+        }
+
+        public double Total()
+        {
+            double sum = 0;
+            foreach (OrderItem item in Items)
+            {
+                sum += item.SubTotal();
+            }
+            return sum;
+        }
+    }
+}
