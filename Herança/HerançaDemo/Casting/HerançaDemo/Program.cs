0ec3fb80634b46d967e49c130be9e8f62069dfc1
Thiago Teixeira <60077182+ThiagoTA@users.noreply.github.com>
2022-01-10 14:29:33 -0300

    Add files via upload

---
+++ Herança/HerançaDemo/Casting/HerançaDemo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using HerançaDemo.Entities;
+
+namespace HerançaDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account acc = new Account(1001, "Alex", 0.0);
+            BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+
+            // UPCASTING
+
+            Account acc1 = bacc;
+            Account acc2 = new BusinessAccount(1003, "Bob", 1000.0, 200.00);
+            Account acc3 = new SavingsAccount(1004, "Ana", 0.0, 0.01);
+
+            // DOWNCASTING
+
+            BusinessAccount acc4 = (BusinessAccount)acc2;
+            acc4.Loan(100);
+            Console.WriteLine(acc4.Balance);
+
+            //BusinessAccount acc5 = (BusinessAccount)acc3;
+            if(acc3 is BusinessAccount)
+            {
+                //BusinessAccount acc5 = (BusinessAccount)acc3;
+                BusinessAccount acc5 = acc3 as BusinessAccount;
+                acc5.Loan(200.00);
+                Console.WriteLine("Loan!");
+            }
+
+            if(acc3 is SavingsAccount)
+            {
+                //SavingsAccount acc5 = (SavingsAccount)acc3;
+                SavingsAccount acc5 = acc3 as SavingsAccount;
+                acc5.UpdateBalance();
+                Console.WriteLine("Update!");
+            }
+        }
+    }
+}
