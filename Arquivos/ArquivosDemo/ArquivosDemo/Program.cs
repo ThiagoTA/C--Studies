218dc47b022f5b84182b00224da4981503acc71d
Thiago Teixeira <60077182+ThiagoTA@users.noreply.github.com>
2022-02-08 09:00:48 -0300

    Add files via upload

---
+++ Arquivos/ArquivosDemo/ArquivosDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace ArquivosDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\temp\file1.txt";
+            string targetPath = @"C:\temp\file2.txt";
+            string[] lines = File.ReadAllLines(sourcePath);
+
+            try
+            {
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+
+                // FileInfo fileInfo = new FileInfo(sourcePath);
+                // fileInfo.CopyTo(targetPath);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
