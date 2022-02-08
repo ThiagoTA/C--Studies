0c83b5c47ce0c84fa5c7ac9cc15491e6e835dc9d
Thiago Teixeira <60077182+ThiagoTA@users.noreply.github.com>
2022-02-08 10:17:34 -0300

    Add files via upload

---
+++ Arquivos/ArquivosDemo/ArquivosDemo5/ArquivosDemo5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+string path = @"c:\myfolder";
+
+try
+{
+    var folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+    Console.WriteLine("Folders:");
+    foreach (string s in folders)
+    {
+        Console.WriteLine(s);
+    }
+
+    var files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+    Console.WriteLine("Files:");
+    foreach (string s in files)
+    {
+        Console.WriteLine(s);
+    }
+
+    Directory.CreateDirectory(path + @"\newfolder");
+}
+catch (IOException e)
+{
+    Console.WriteLine("An error occurred");
+    Console.WriteLine(e.Message);
+}
