8ae9f0fdabe2433bf847f15b052f7812b83378c5
Thiago Teixeira <60077182+ThiagoTA@users.noreply.github.com>
2022-02-10 13:10:22 -0300

    Add files via upload

---
+++ Arquivos/ArquivosDemo/ExProposto2/ExProposto2/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExProposto2
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Amount { get; set; }
+
+        public double Total()
+        {
+            return Price * Amount;
+        }
+    }
+}
---
+++ Arquivos/ArquivosDemo/ExProposto2/ExProposto2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Globalization;
+
+Console.Write("Enter file full path: ");
+string sourceFilePath = Console.ReadLine();
+
+try
+{
+    string[] lines = File.ReadAllLines(sourceFilePath);
+
+    string sourceFolderPath = Path.GetDirectoryName(sourceFilePath);
+    string targetfolderPath = sourceFilePath + @"\out";
+    string targetFilePath = targetfolderPath + @"summary.csv";
+
+    Directory.CreateDirectory(targetFilePath);
+
+    using (StreamWriter sw = File.AppendText(targetFilePath))
+    {
+        foreach (string line in lines)
+        {
+            string[] fields = line.Split(',');
+            string name = fields[0];
+            double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
+            int amount = int.Parse(fields[2]);
+
+            
+        }
+    }
+} catch
+{
+
+}
