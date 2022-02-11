7a027379e9b7581dba709b83d0a394710a92ad77
Thiago Teixeira <60077182+ThiagoTA@users.noreply.github.com>
2022-02-11 10:29:36 -0300

    Add files via upload

---
+++ Interfaces/InterfaceDemo/InterfaceDemo/Entities/Vehicle.cs
@@ -0,0 +1,12 @@
+﻿namespace InterfaceDemo.Entities
+{
+    class Vehicle
+    {
+        public string Model { get; set; }
+
+        public Vehicle(string model)
+        {
+            Model = model;
+        }
+    }
+}
---
+++ Interfaces/InterfaceDemo/InterfaceDemo/Services/RentalService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using InterfaceDemo.Entities;
+
+namespace InterfaceDemo.Services
+{
+    internal class RentalService
+    {
+        public double PricePerhour { get; private set; }
+        public double PricePerDay { get; private set; }
+
+        private ITaxService _taxService;
+
+        public RentalService(double pricePerhour, double pricePerDay, ITaxService taxService)
+        {
+            PricePerhour = pricePerhour;
+            PricePerDay = pricePerDay;
+            _taxService = taxService;
+        }
+
+        public void ProcessInvoice(CarRental carRental)
+        {
+            TimeSpan duration = carRental.Finish.Subtract(carRental.Start);
+            
+            double basicPayment = 0.0;
+            if (duration.TotalHours <= 12.0)
+            {
+                basicPayment = PricePerhour * Math.Ceiling(duration.TotalHours);
+            } else
+            {
+                basicPayment = PricePerDay * Math.Ceiling(duration.TotalDays);
+            }
+
+            double tax = _taxService.Tax(basicPayment);
+
+            carRental.Invoice = new Invoice(basicPayment, tax);
+        }
+    }
+}
